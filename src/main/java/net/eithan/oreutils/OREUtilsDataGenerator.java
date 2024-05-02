d627380fc863dfa8c639b69e64e88ae781b90266
Eithan Zur <eithanz713@gmail.com>
2024-05-02 22:44:30 +0300

    first commit

---
+++ src/main/java/net/eithan/oreutils/OREUtilsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.eithan.oreutils;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OREUtilsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
